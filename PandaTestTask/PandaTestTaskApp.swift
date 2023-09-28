4b85a82d36cdf3311e398ca165c0c1cd1ad601bc
Dmytro Ukrainskyi <dmytro.ukrainskyi@gmail.com>
2023-09-28 23:16:40 +0300

    Initial Commit

---
+++ PandaTestTask/PandaTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaTestTaskApp.swift
+//  PandaTestTask
+//
+//  Created by Dmytro Ukrainskyi on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PandaTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
